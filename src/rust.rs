e646eb977ffe47b6d56a3158d59ada7de54a0ac0
Charles <8595764@qq.com>
2023-03-23 19:48:10 +0800

    rewrite cargo

--- src/rust.rs
+++ src/rust.rs
@@ -1,71 +1,102 @@
 use std::{
-    env,
-    fs::File,
-    io::{Read, Write},
-    path::PathBuf,
-    process::Command,
+    fs,
+    io::{BufRead, BufReader, Write},
+    path::Path,
 };
 
 pub fn set() {
-    // 镜像地址
-    let mirror_url = &String::from("https://mirrors.tuna.tsinghua.edu.cn/git/cargo");
-
     // 获取 Cargo 配置文件路径
-    let mut home_dir = match env::var_os("HOME") {
-        Some(path) => PathBuf::from(path),
-        None => PathBuf::from("."),
-    };
-    home_dir.push(".cargo");
-    home_dir.push("config");
-
-    // 写入镜像地址到 Cargo 配置文件
-    let mut file = match File::open(&home_dir) {
-        Ok(file) => file,
-        Err(_) => {
-            // 如果文件不存在，则创建一个新文件
-            match File::create(&home_dir) {
-                Ok(file) => file,
-                Err(err) => {
-                    println!("Failed to create file {}: {}", home_dir.display(), err);
-                    return;
-                }
-            }
-        }
-    };
-
-    let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Ok(_) => (),
-        Err(err) => {
-            println!("Failed to read file {}: {}", home_dir.display(), err);
+    let home_dir = match dirs::home_dir() {
+        Some(path) => path,
+        None => {
+            eprintln!("Error: could not find home directory.");
             return;
         }
+    };
+    let cargo_config_path = home_dir.join(".cargo").join("config");
+
+    // 如果配置文件不存在，则创建一个空的配置文件
+    if !Path::new(&cargo_config_path).exists() {
+        fs::create_dir_all(cargo_config_path.parent().unwrap()).unwrap();
+        fs::File::create(&cargo_config_path).unwrap();
     }
 
-    contents = contents.replace(
-        "[source.crates-io]\nregistry = \"https://crates.io\"\n",
-        &format!("[source.crates-io]\nregistry = \"{}\"\n", mirror_url),
-    );
-    match file.write_all(contents.as_bytes()) {
-        Ok(_) => println!("Successfully set mirror to {}", mirror_url),
-        Err(err) => println!("Failed to write to file {}: {}", home_dir.display(), err),
+    // 读取配置文件内容
+    let file = fs::File::open(&cargo_config_path).unwrap();
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
+
+    // 查找 registry 的位置，并替换为镜像
+    let mut new_lines: Vec<String> = Vec::new();
+    let mut found_registry = false;
+    for line in lines {
+        if line.starts_with(r#"[registry."#) {
+            new_lines.push("[registry.crates-io]".to_string());
+            new_lines.push(
+                "index = \"https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git\""
+                    .to_string(),
+            );
+            found_registry = true;
+        } else {
+            new_lines.push(line);
+        }
     }
+    if !found_registry {
+        new_lines.push("[registry.crates-io]".to_string());
+        new_lines.push(
+            "index = \"https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git\"".to_string(),
+        );
+    }
+
+    // 将新的配置写入文件
+    let mut file = fs::File::create(&cargo_config_path).unwrap();
+    for line in new_lines {
+        writeln!(file, "{}", line).unwrap();
+    }
+
+    println!("Successfully set the Cargo mirror.");
 }
 
 pub fn unset() {
-    let home_dir = env::var("HOME").expect("failed to get home directory");
-    let cargo_config_path = format!("{}/.cargo/config", home_dir);
+    // 获取 Cargo 配置文件路径
+    let home_dir = match dirs::home_dir() {
+        Some(path) => path,
+        None => {
+            eprintln!("Error: could not find home directory.");
+            return;
+        }
+    };
+    let cargo_config_path = home_dir.join(".cargo").join("config");
 
-    let output = Command::new("sed")
-        .arg("-i")
-        .arg("/\\[registry\\.mirrors\\]/,/^\\[/ { /^[^#]/ s/^/#/ }")
-        .arg(&cargo_config_path)
-        .output()
-        .expect("failed to execute process");
-
-    if output.status.success() {
-        println!("Successfully deleted Cargo registry mirror.");
-    } else {
-        println!("Failed to delete Cargo registry mirror.");
+    // 如果配置文件不存在，则退出
+    if !Path::new(&cargo_config_path).exists() {
+        eprintln!("Error: could not find Cargo configuration file.");
+        return;
     }
+
+    // 读取配置文件内容
+    let file = fs::File::open(&cargo_config_path).unwrap();
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
+
+    // 查找 registry 部分，并删除它
+    let mut new_lines: Vec<String> = Vec::new();
+    let mut in_registry = false;
+    for line in lines {
+        if line.starts_with(r#"[registry."#) {
+            in_registry = true;
+        } else if in_registry && line.starts_with('[') {
+            in_registry = false;
+        } else if !in_registry {
+            new_lines.push(line);
+        }
+    }
+
+    // 将新的配置写入文件
+    let mut file = fs::File::create(&cargo_config_path).unwrap();
+    for line in new_lines {
+        writeln!(file, "{}", line).unwrap();
+    }
+
+    println!("Successfully removed the Cargo mirror.");
 }
